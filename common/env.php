b6888ad134df1a54186c144e884afe763cc591fc
Aekan <alpe15.1992@gmail.com>
2019-02-25 13:07:37 +0100

    update for react uis

--- common/env.php
+++ common/env.php
@@ -1,3 +1,8 @@
+
+<script src="https://unpkg.com/react@16/umd/react.development.js"></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+
 <?php
 /**
  * Require helpers
