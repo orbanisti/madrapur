19eae5006b0cb2775424af35fa5be566957e2568
Aekan <alpe15.1992@gmail.com>
2019-03-25 16:29:46 +0100

    react index base

--- frontend/modules/Page/views/page/index.php
+++ frontend/modules/Page/views/page/index.php
@@ -20,8 +20,45 @@
         }	
     }
 
+    class SEOBlock extends React.Component {
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                seoTitle: props.title
+            };
+        }
+        render() {
+            return (
+                <div className="row seo-block" >
+                    <h1 className="col-lg-12">
+                        {this.state.seoTitle}
+                    </h1>
+                </div>
+            );
+        }
+    }
+
+    class HomePage extends Page {
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                title: "asdasdasdasd"
+            };
+        }
+
+        render() {
+            return (
+                <div className="container">
+                    <SEOBlock title={this.state.title} />
+                </div>
+            );
+        }
+    }
+
     ReactDOM.render(
-        <Page />,
+        <HomePage />,
         document.getElementById('root')
     );
 </script>
