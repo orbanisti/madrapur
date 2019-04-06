f82a2cf04d7e9963a144fd9678550309f90209e5
orbanisti <orbanisti94@gmail.com>
2019-04-06 04:37:47 +0200

    hotfix

--- backend/modules/Product/views/product/admin.php
+++ backend/modules/Product/views/product/admin.php
@@ -14,7 +14,7 @@ use backend\modules\Product\models\Product;
 
 <?php
 
-    $client = new \nizsheanez\jsonRpc\Client('http://www.api.localhost.com/v1/worker/');
+    $client = new \nizsheanez\jsonRpc\Client('http://modulus.hu/v1/worker/');
 
     $response = $client->product(43);
 
