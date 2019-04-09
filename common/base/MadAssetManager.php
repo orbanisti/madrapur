99b862284531cc4d0f7ba297a61bbf521a323fc9
Aekan <alpe15.1992@gmail.com>
2019-04-09 15:15:06 +0200

    introduced MadAssetManager

---
+++ common/base/MadAssetManager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ROG
+ * Date: 2019. 04. 09.
+ * Time: 11:44
+ */
+
+namespace common\base;
+
+
+use yii\web\AssetManager;
+
+class MadAssetManager extends AssetManager {
+    public function hashCallback($path) {
+        return $path;
+    }
+}
