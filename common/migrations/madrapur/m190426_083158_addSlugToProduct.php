c49c1903356213d724a4b9f70a999b1654d85aef
Aekan <alpe15.1992@gmail.com>
2019-05-02 09:33:55 +0200

    * slug migration
    *

--- common/migrations/madrapur/m190426_083158_addSlugToProduct.php
+++ common/migrations/madrapur/m190426_083158_addSlugToProduct.php
@@ -4,7 +4,7 @@ use yii\db\Schema;
 use yii\db\Migration;
 
 class m190426_083158_addSlugToProduct extends Migration {
-    public $tableName = "modulusproducts";
+    public $tableName = "modulusProducts";
     public $columnName = "slug";
 
     public function safeUp() {
@@ -12,6 +12,13 @@ class m190426_083158_addSlugToProduct extends Migration {
             $this->tableName,
             'slug',
             'varchar(255)'
+        );
+
+        $this->update(
+            $this->tableName,
+            [
+                'slug' => 'testSlug'
+            ]
         );
     }
 
