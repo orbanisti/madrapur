9f336de186881c54877ec0e5e5c3dac00d31c29d
orbanisti <orbanisti94@gmail.com>
2019-05-02 11:55:33 +0200

    Slugify function included in ProductController (slug is saved on edit)

--- backend/modules/Product/controllers/ProductController.php
+++ backend/modules/Product/controllers/ProductController.php
@@ -77,6 +77,35 @@ class ProductController extends Controller {
     /**
      * @return string
      */
+
+
+    function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // remove duplicate -
+        $text = preg_replace('~-+~', '-', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+
     public function actionUpdate()
     {
 
@@ -519,6 +548,10 @@ class ProductController extends Controller {
                     $modelEvents[] = $event;
                 }
             }
+
+    if($model->slug=='testSlug' || $model->slug==''){
+        $model->slug=$this->slugify($model->title);
+    }
 
 
             return $this->render(
