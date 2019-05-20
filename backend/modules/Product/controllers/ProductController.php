1318d2a3a2b7ce3c8c57763e94f77e9e7b56f474
orbanisti <orbanisti94@gmail.com>
2019-05-20 19:17:36 +0200

    hotfix2 ;

--- backend/modules/Product/controllers/ProductController.php
+++ backend/modules/Product/controllers/ProductController.php
@@ -709,7 +709,7 @@ class ProductController extends Controller {
                         $curl=curl_init($curlUrl);
                         curl_setopt($curl, CURLOPT_HEADER, 0);
                         curl_setopt($curl, CURLOPT_VERBOSE, 0);
-                        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);gi
+                        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
                         $response=curl_exec($curl);
 
 
