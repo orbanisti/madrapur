a3e7bb5babfe5cb93cb3046f8b8ce15dc6f642b4
orbanisti <orbanisti94@gmail.com>
2019-06-27 16:54:57 +0200

    Welcome mail wip

--- backend/modules/Modmail/controllers/ModmailController.php
+++ backend/modules/Modmail/controllers/ModmailController.php
@@ -18,22 +18,28 @@ class ModmailController extends Controller {
     public function actionAdmin() {
         $model=new Modmail();
         $postedMail=Yii::$app->request->post('Modmail');
-
+        $newUsername=Yii::$app->user->getIdentity()->username;
+        $bootstrap='<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
+        $welcomeHTML="<div class=\"container\">
+      <div class=\"row\">
+        <div class=\"col-lg-12 text-center\">
+          
+          
+        <div class=\"jumbotron\" style=\"\">		  <h1 class=\"display-3\">Hello, $newUsername!</h1>		  <p class=\"lead\">Your Madrapur account has been created, you can log in following the button below</p>		  <hr class=\"my-4\">		  		  <p class=\"lead\">			<a class=\"btn btn-primary btn-lg\" href=\"#\" role=\"button\">Log in</a>		  </p>		</div></div>
+      </div>
+    </div>";
 
 
         if($postedMail){
-
             $to = $postedMail['to'];
             $from=$postedMail['from'];
             $subject = $postedMail['subject'];
-            $txt = $postedMail['body'];
+            $txt = $bootstrap.$welcomeHTML;
             $headers = "From: $from" . "\r\n";
 
             if(mail($to,$subject,$txt,$headers)){
                 Modmail::insertOne($model,$postedMail);
             }
-
-
         }
 
         $searchModel = new Modmail();
@@ -47,9 +53,6 @@ class ModmailController extends Controller {
             'type',
 
         ];
-
-
-
 
         return $this->render('admin',['model'=>$model,'dataProvider'=>$dataProvider,'searchModel'=>$searchModel,'gridColumns'=>$gridColumns]);
     }
