f5594155b3960a9df2bc9fdfc1b064808694182b
orbanisti <orbanisti94@gmail.com>
2019-07-03 10:18:09 +0200

    Email sending 0.6

--- backend/modules/Modmail/controllers/ModmailController.php
+++ backend/modules/Modmail/controllers/ModmailController.php
@@ -36,7 +36,8 @@ class ModmailController extends Controller {
             $subject = $postedMail['subject'];
             $txt = $bootstrap.$welcomeHTML;
             $headers = "From: $from" . "\r\n";
-
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
             if(mail($to,$subject,$txt,$headers)){
                 Modmail::insertOne($model,$postedMail);
             }
