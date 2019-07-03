27c5be829a4f46e57792aba310d2e4b2a5104ea7
orbanisti <orbanisti94@gmail.com>
2019-07-03 14:22:50 +0200

    fix block

--- backend/modules/Product/controllers/ProductController.php
+++ backend/modules/Product/controllers/ProductController.php
@@ -799,6 +799,8 @@ class ProductController extends Controller {
             Yii::error('date:' . $date);
             $curlUrl = $myurl . '/wp-json/blocktime/v1/date/' . date('Y-m-d', strtotime($date)) . '/time/' . date('H:i', strtotime($date)) . '/id/' . $myprodid;
 
+            echo '<input type="hidden" value="'.$curlUrl.'"  name="currentCurlUrl"/>';g
+
             Yii::error('blockUrl:' . $curlUrl);
             $curl = curl_init($curlUrl);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
