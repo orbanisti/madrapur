1bee093abd00a40e5c4b5537eaf30a23e7d66b25
Aekan <alpe15.1992@gmail.com>
2019-08-22 12:09:43 +0200

    mail #2

--- backend/modules/Modmail/models/Modmail.php
+++ backend/modules/Modmail/models/Modmail.php
@@ -121,7 +121,7 @@ class Modmail extends MadActiveRecord {
 
         $postedData['body'] = $txt;
 
-        $txt = $bootstrap;//.$welcomeHTML;
+        $txt = $bootstrap.$txt;//.$welcomeHTML;
         $headers = "From: $from" . "\r\n";
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
