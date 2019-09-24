57d967a6b638c5887e070107c059b0f67cbaae5b
orbanisti <orbanisti94@gmail.com>
2019-09-24 11:55:29 +0200

    hotsave

--- backend/modules/Reservations/views/reservations/mytransactions.php
+++ backend/modules/Reservations/views/reservations/mytransactions.php
@@ -1,216 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ROG
- * Date: 2019. 02. 05.
- * Time: 20:38
- */
 
-use backend\components\extra;
-    use backend\modules\Reservations\models\Reservations;
-    use kartik\helpers\Html;
+    use backend\modules\Product\models\ProductSource;
+
+    use kartik\grid\GridView;
+    use yii\helpers\Html;
+    use yii\helpers\Url;
     use kartik\icons\Icon;
 
 ?>
 
-
-<?php
-
-    $gridColumns = [
-        'id',
-
-        # 'bookingId',
-
-        [
-            'label' => 'Seller',
-            'attribute'=>'sellerName'
-        ],
-        [
-            'label' => 'Cost',
-            'attribute'=>'bookingCost'
-        ],
-        [
-            'label' => 'Currency',
-            'attribute'=>'orderCurrency'
-        ],
-        [
-            'label' => 'Persons',
-            'attribute'=>'bookedChairsCount'
-        ],
-        [
-            'label' => 'Invoice date',
-            'attribute' => 'invoiceDate',
-            'filter' => \yii\jui\DatePicker::widget([
-                'model' => new Reservations(),
-                'attribute' => 'invoiceDate',
-                'dateFormat' => 'yyyy-MM-dd',
-                'clientOptions' => [
-                    'autoClose' => true
-                ]
-            ]),
-            'format' => 'raw',
-        ],
-        #g 'updateDate',
-
-        # 'bookingDate',
-        [
-            'label' => 'Edit Booking',
-            'format' => 'html',
-            'value' => function ($model) {
-                return '<a href="/Reservations/reservations/bookingview?id=' . $model->returnId() . '">View' . '</a>';
-            }
-        ],
-
-    ];
-    $gridColumns2 = [
-//        'updateDate',
-//        'invoiceDate',
-
-        [
-            'label' => 'Persons',
-            'attribute'=>'bookedChairsCount',
-            'format'=>'html',
-            'value'=>function($model){
-            $sellerBadge='';
-            if(isset($model->iSellerName)){
-
-                $sellerBadge=" <span class=\" badge bg-yellow\">".  $model->sellerName."</span>";
-            }
-
-            return $model->bookedChairsCount.' '.Icon::show('users', ['class'=>'fa-lg','framework'
-                =>Icon::FA]).$sellerBadge;
-            }
-        ],
-    [
-        'label' => 'Cost',
-        'attribute'=>'bookingCost',
-
-         'format'=>'html',
-            'value'=>function($model){
-
-            if($model->orderCurrency=='EUR'){
-                $currencySymbol=Icon::show('euro', ['class'=>'fa-lg','framework'
-                =>Icon::FA]);
-            }else{
-                $currencySymbol='Ft';
-            }
-
-            return $model->bookingCost.' '.$currencySymbol;
-            }
-    ],
-
-
-
-    ];
-    $grid=\kartik\grid\GridView::widget([
-
-                'dataProvider' => $dataProvider,
-
-                'columns' => $gridColumns,
-                'pjax' => false,
-                'toolbar' => [
-                    [
-
-                        'options' => ['class' => 'btn-group mr-2']
-                    ],
-                    '{export}',
-                    '{toggleData}',
-                ],
-                'toggleDataContainer' => ['class' => 'btn-group mr-2'],
-                // set export properties
-                'export' => [
-                    'fontAwesome' => true,
-
-                ],
-                'bordered' => true,
-                'striped' => true,
-                'panel' => [
-                    'heading' => '<i class="fa fa-calendar"></i>',
-
-                ],
-
-            ]);
-
-    $gridAlltransactions=\kartik\grid\GridView::widget([
-
-        'dataProvider' => $allTransactionData,
-        'columns' => $gridColumns2,
-        'pjax' => false,
-        'layout' => '{items}',
-        'toolbar' => [
-            [
-
-                'options' => ['class' => 'btn-group mr-2']
-            ],
-            '{export}',
-            '{toggleData}',
-        ],
-        'toggleDataContainer' => ['class' => 'btn-group mr-2'],
-        // set export properties
-        'export' => [
-            'fontAwesome' => true,
-
-        ],
-
-
-
-    ]);
-    $gridEurtransactions=\kartik\grid\GridView::widget([
-
-        'dataProvider' => $eurTransactionData,
-        'columns' => $gridColumns2,
-        'pjax' => false,
-        'layout' => '{items}',
-        'toolbar' => [
-            [
-
-                'options' => ['class' => 'btn-group mr-2']
-            ],
-            '{export}',
-            '{toggleData}',
-        ],
-        'toggleDataContainer' => ['class' => 'btn-group mr-2'],
-        // set export properties
-        'export' => [
-            'fontAwesome' => true,
-
-        ],
-
-
-    ]);
-  $gridHuftransactions=\kartik\grid\GridView::widget([
-
-        'dataProvider' => $hufTransactionData,
-        'columns' => $gridColumns2,
-        'pjax' => false,
-        'layout' => '{items}',
-        'toolbar' => [
-            [
-
-                'options' => ['class' => 'btn-group mr-2']
-            ],
-            '{export}',
-            '{toggleData}',
-        ],
-        'toggleDataContainer' => ['class' => 'btn-group mr-2'],
-        // set export properties
-        'export' => [
-            'fontAwesome' => true,
-
-        ],
-
-
-    ]);
-
-    $today=date("Y-m-d");
-
-    $eurToday=Reservations::sumDataProvider($eurTransactionData->models, 'bookingCost');
-    $hufToday=Reservations::sumDataProvider($hufTransactionData->models, 'bookingCost');
-
-            ?>
-
-<!--suppress ALL -->
-<div class="products-index">
 
 <div class="row">
     <div class="col-12">
@@ -218,8 +16,8 @@ use backend\components\extra;
         <div class="card card-primary card-outline">
             <div class="card-header">
                 <h3 class="card-title">
-                    <i class="far fa-chart-bar"></i>
-                    my Transactions
+                    <i class="fas fa-table  "></i>
+                    Reporting Center <?=$today?>
                 </h3>
 
                 <div class="card-tools">
@@ -229,156 +27,45 @@ use backend\components\extra;
                 </div>
             </div>
             <div class="card-body">
+                <div class="box box-widget widget-user">
+                    <?php
+                        /**
+                         * Json RPC Communication TODO Close this hole asap
+                         */
+                        /*
+                         * Example of Yell function of Api Rester returns free spaces of productId on selecetedDate in Int
+                            $client = new \nizsheanez\jsonRpc\Client('http://www.api.localhost.com/v1/worker');
+                            $currentProduct=44;
 
-                <div class="row">
-                    <div class="col-12">
-                        <!-- interactive chart -->
-                            <div id="eurTrans"class="card card-primary card-outline">
-                            <div class="card-header">
-                                <h3 class="card-title">
-                                    <i class="fas fa-euro-sign  "></i>
-                                    EUR Transactions
-                                </h3>
+                            $selectedDate=date("Y-m-d");
 
-                                <div class="card-tools">
-                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                                    </button>
-
-                                </div>
-                            </div>
-                            <div class="card-body">
+                            $response = $client->yell($selectedDate,$currentProduct);
+                            echo $response;
+                        */
 
 
-                            </div>
-                            <!-- /.card-body-->
-                        </div>
-                            <div id="hufTrans"class="card card-primary card-outline">
-                            <div class="card-header">
-                                <h3 class="card-title">
-                                    <i class="fas fa-coins  "></i>
-                                    HUF Transactions
-                                </h3>
-
-                                <div class="card-tools">
-                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                                    </button>
-
-                                </div>
-                            </div>
-                            <div class="card-body">
-
-
-                            </div>
-                            <!-- /.card-body-->
-                        </div>
-                            <div id="allTrans"class="card card-primary card-outline">
-                            <div class="card-header">
-                                <h3 class="card-title">
-                                    <i class="fas fa-money-bill  "></i>
-                                    All Transactions
-                                </h3>
-
-                                <div class="card-tools">
-                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                                    </button>
-
-                                </div>
-                            </div>
-                            <div class="card-body">
-
-
-                            </div>
-                            <!-- /.card-body-->
-                        </div>
-                        <!-- /.card -->
-
-                    </div>
-
-                    <!-- /.col -->
-                </div>
-
-
-                <div class="row">
-
-                    <div class="col-lg-12">
-
-                        <div class="box box-widget widget-user">
-                            <!-- Add the bg color to the header using any of the bg-* classes -->
+                    ?>
 
 
 
+                        <?php
+                            foreach ($userList as $userblock){
+                                echo $userblock;
+                            }
 
-                            <div class="card card-widget widget-user-2">
-                                <!-- Add the bg color to the header using any of the bg-* classes -->
-
-                                <div class="card-footer p-0">
-                                    <ul class="nav flex-column">
-                                        <li><a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" class="collapsed" href="#">
-                                                <span class="btn bg-aqua">ALL</span>  income today
-                                                <span class="float-right badge bg-green"><?=Icon::show('euro', ['class'=>'fa-lg','framework'
-                                                    =>Icon::FA])?><?=$eurToday?></span>
-                                                <span class="float-right badge bg-green"><?=$hufToday?> Ft</span></a></li>
-                                        <div id="collapseOne" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
-                                            <div class="box-body">
-                                                <?=$gridAlltransactions?>
-                                            </div>
-                                        </div>
-
-                                        <li><a data-toggle="collapse" data-parent="#accordion" href="#collapseEur" aria-expanded="false" class="collapsed" href="#">
-                                                <span class="btn bg-info">EUR</span>  income today <span
-                                                        class="float-right badge bg-green"><?=Icon::show('euro', ['class'=>'fa-lg','framework'
-                                                    =>Icon::FA])?><?=$eurToday?></span></a></li>
-                                        <div id="collapseEur" class="panel-collapse collapse" aria-expanded="false" style="height:
-                        0px;">
-                                            <div class="box-body">
-                                                <?=$gridEurtransactions?>
-                                            </div>
-                                        </div>
-
-                                        <li><a data-toggle="collapse" data-parent="#accordion" href="#collapseHuf" aria-expanded="false" class="collapsed" href="#">
-                                                <span class="btn bg-aqua">HUF</span> income today     <span class="float-right badge bg-green"><?=$hufToday?> Ft</span></a></li>
-                                        <div id="collapseHuf" class="panel-collapse collapse" aria-expanded="false" style="height:
-                        0px;">
-                                            <div class="box-body">
-                                                <?=$gridHuftransactions?>
-                                            </div>
-                                        </div>
-
-                                    </ul>
-                                </div>
-                            </div>
-
-                        </div>
-
-
-
-
-                    </div>
-                    <div class="col-lg-8 col-sm-12 hidden"><?=$grid?></div>
+                        ?>
 
 
 
 
                 </div>
 
-               
             </div>
             <!-- /.card-body-->
         </div>
         <!-- /.card -->
 
-    </div>   
- 
+    </div>
+
     <!-- /.col -->
-</div>
-
-
-
-
-
-    <script>
-
-
-    </script>
-
 </div>
