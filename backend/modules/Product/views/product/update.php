2dfce309f0d6113a5cc74bdac17cb7c2e6e157a4
orbanisti <orbanisti94@gmail.com>
2019-09-26 12:34:47 +0200

    Admin 0.3.2

--- backend/modules/Product/views/product/update.php
+++ backend/modules/Product/views/product/update.php
@@ -4,52 +4,76 @@
 
 /* @var $model backend\modules\Products\models\Products */
 
-$this->title = Yii::t('app', 'Termék szerkesztése') . ' <u>' . $model->title . '</u>';
 
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Termékek'), 'url' => ['admin']];
-
-$this->params['breadcrumbs'][] = Yii::t('app', 'Szerkesztés');
 
 ?>
 
-<div class="products-update">
+
+<div class="row">
+    <div class="col-12">
+        <!-- interactive chart -->
+        <div class="card card-primary card-outline">
+            <div class="card-header">
+                <h3 class="card-title">
+                    <i class="fas fa-pencil-alt  "></i>
+                    <?=Yii::t('app', 'Termék szerkesztése') . ' <u>' . $model->title . '</u>'?>
+                </h3>
+
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                    </button>
+
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="products-update">
 
 
-    <h1><?= $this->title ?></h1>
+                    <h1><?= $this->title ?></h1>
 
-    <?php
+                    <?php
 
-    switch ($updateResponse) {
-        case 1:
-            $updateResponse = '<span style="color:green">Product Update Successful</span>';
-            break;
-        case 0:
-            $updateResponse = '<span style="color:red">Product Update Failed</span>';
-    }
-    echo $updateResponse;
-    ?>
-
-
-    <?=
-
-    $this->render('editForm', [
-        'model' => $model,
-        'backenddata' => $backendData,
-        'prodId' => $prodId,
-        'modelTimes' => $modelTimes,
-        'modelPrices' => $modelPrices,
-        'modelEvents' => $modelEvents,
-        'modelSources' => $modelSources,
-
-        /*
-                'modelTranslations' => $modelTranslations,
-
-                'modelPrices' => $modelPrices,
-                'modelTimes' => $modelTimes
-
-        */
-    ]) ?>
+                        switch ($updateResponse) {
+                            case 1:
+                                $updateResponse = '<span style="color:green">Product Update Successful</span>';
+                                break;
+                            case 0:
+                                $updateResponse = '<span style="color:red">Product Update Failed</span>';
+                        }
+                        echo $updateResponse;
+                    ?>
 
 
+                    <?=
+
+                        $this->render('editForm', [
+                            'model' => $model,
+                            'backenddata' => $backendData,
+                            'prodId' => $prodId,
+                            'modelTimes' => $modelTimes,
+                            'modelPrices' => $modelPrices,
+                            'modelEvents' => $modelEvents,
+                            'modelSources' => $modelSources,
+
+                            /*
+                                    'modelTranslations' => $modelTranslations,
+
+                                    'modelPrices' => $modelPrices,
+                                    'modelTimes' => $modelTimes
+
+                            */
+                        ]) ?>
+
+
+                </div>
+               
+            </div>
+            <!-- /.card-body-->
+        </div>
+        <!-- /.card -->
+
+    </div>   
+ 
+    <!-- /.col -->
 </div>
 
