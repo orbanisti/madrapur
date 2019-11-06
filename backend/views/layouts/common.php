fe0ff1d66316bfab575215bcb2d89a2ad5b41049
orbanisti <orbanisti94@gmail.com>
2019-11-06 15:51:24 +0100

    adminlte fix for sidebar, use rc6, as 3.0 breaks card colors

--- backend/views/layouts/common.php
+++ backend/views/layouts/common.php
@@ -806,9 +806,9 @@ $bundle = BackendAsset::register($this);
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar  sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar  sidebar-dark-primary elevation-4 ">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="/" class="brand-link">
             <!--img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8"-->
             <span class="brand-text font-weight-light">Madrapur</span>
