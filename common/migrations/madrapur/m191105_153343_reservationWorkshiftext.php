3f4810de0bdf52722d99d538e1a51913c4c956eb
orbanisti <orbanisti94@gmail.com>
2019-11-12 11:16:29 +0100

    workshift migration fix migrate down n up

--- common/migrations/madrapur/m191105_153343_reservationWorkshiftext.php
+++ common/migrations/madrapur/m191105_153343_reservationWorkshiftext.php
@@ -4,25 +4,19 @@ use yii\db\Schema;
 use yii\db\Migration;
 
 class m191105_153343_reservationWorkshiftext extends Migration {
-    public $tableName = "tableName";
-    public $tableOptions = "";
+    public $tableName = "modulusBookings";
+    public $columnName = "workshiftId";
 
     public function safeUp() {
-        if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $this->tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
-        }
-
-        $this->createTable(
-            $this->tableName, [
-                'id' => $this->primaryKey(),
-                'title' => $this->string(12)->notNull()->unique(),
-                'body' => $this->text(),
-            ], $this->tableOptions
+        $this->addColumn(
+            $this->tableName,
+            $this->columnName,
+            $this->bigInteger(13)->defaultValue(null)
         );
+
     }
 
     public function safeDown() {
-        $this->dropTable($this->tableName);
+        $this->dropColumn($this->tableName, $this->columnName);
     }
 }
