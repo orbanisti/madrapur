026d280a80aca0d9a820e6af3b192330fe599a20
orbanisti <orbanisti94@gmail.com>
2020-01-08 16:58:12 +0100

    hotfix

--- backend/modules/Product/models/ProductAddonBlockout.php
+++ backend/modules/Product/models/ProductAddonBlockout.php
@@ -21,7 +21,7 @@ class ProductAddonBlockout extends \yii\db\ActiveRecord
      */
     public static function tableName()
     {
-        return 'modulusaddonblockouts';
+        return 'modulusAddonBlockouts';
     }
 
     /**
