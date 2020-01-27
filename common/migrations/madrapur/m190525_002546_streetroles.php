417f3f364c50c3d6aa07300744d41ea8ef0a8dfd
orbanisti <orbanisti94@gmail.com>
2020-01-27 10:19:16 +0100

    hotfix

--- common/migrations/madrapur/m190525_002546_streetroles.php
+++ common/migrations/madrapur/m190525_002546_streetroles.php
@@ -1,124 +1,30 @@
 <?php
 
-use yii\db\Schema;
-use yii\db\Migration;
+    use yii\db\Schema;
+    use yii\db\Migration;
 
-class m190525_002546_streetroles extends Migration {
-
-
-
-    private $_roles = [
-        User::ROLE_HOTEL_SELLER=> 'Hotel seller role'
-
-    ];
-
-    private $_permissions = [
-        User::ASSIGN_HOTEL_SELLER=> 'Assign Hotel seller role',
+    class m190525_002546_streetroles extends Migration {
 
 
-    ];
+        public function safeUp() {
+            $auth = Yii::$app->authManager;
+            $createReservation = $auth->createPermission('createReservation');
+            $createReservation->description = 'Create a Reservation';
+            $auth->add($createReservation);
 
-    private $hotelSellerPermissions = [
-        'loginToBackend',
-
-        Reservations::CREATE_BOOKING,
-        Reservations::EDIT_OWN_BOOKING,
-        Reservations::VIEW_OWN_BOOKINGS,
+            $streetSeller = $auth->createRole('streetSeller');
+            $auth->add($streetSeller);
+            $auth->addChild($streetSeller, $createReservation);
 
 
-    ];
 
-    protected function createPermissions() {
-        foreach ($this->_permissions as $permissionName => $permissionDescription) {
-            self::_createPermission($permissionName, $permissionDescription);
+
+        }
+
+        public function safeDown() {
+            $auth = Yii::$app->authManager;
+
+
+
         }
     }
-
-    private function _createPermission($name, $description = '') {
-        $permission = Yii::$app->authManager->createPermission($name);
-        $permission->description = $description;
-
-        try {
-            Yii::$app->authManager->add($permission);
-        } catch (Exception $e) {
-            Yii::error($e->getMessage(), '_createPermission');
-        }
-    }
-
-    protected function assignAdminPermissions() {
-        $this->assignPermissionsToRole(User::ROLE_ADMINISTRATOR, array_keys($this->_permissions));
-    }
-
-    protected function assignNewRolePermissions() {
-        $this->assignPermissionsToRole(User::ROLE_HOTEL_SELLER, $this->hotelSellerPermissions);
-    }
-
-
-    protected function assignPermissionsToRole($role, $permissions) {
-        foreach ($permissions as $permission) {
-            self::_assignPermissionToRole($role, $permission);
-        }
-    }
-
-    private function _assignPermissionToRole($roleName, $permissionName) {
-        $role = Yii::$app->authManager->getRole($roleName);
-        $permission = Yii::$app->authManager->getPermission($permissionName);
-
-        try {
-            Yii::$app->authManager->addChild($role, $permission);
-        } catch (Exception $e) {
-            Yii::error($e->getMessage(), '_assignPermission');
-        }
-    }
-
-
-
-
-    protected function createRoles() {
-        foreach ($this->_roles as $roleName => $roleDescription) {
-            self::_createRole($roleName, $roleDescription);
-        }
-    }
-    protected function deleteRoles() {
-        foreach ($this->_roles as $roleName => $roleDescription) {
-            self::_deleteRole($roleName, $roleDescription);
-        }
-    }
-
-
-
-    private function _createRole($name, $description = '') {
-        $role = Yii::$app->authManager->createRole($name);
-        $role->description = $description;
-
-        try {
-            Yii::$app->authManager->add($role);
-        } catch (Exception $e) {
-            Yii::error($e->getMessage(), '_createRole');
-        }
-    }
-
-    private function _deleteRole($name, $description = '') {
-        $role = Yii::$app->authManager->getRole($name);
-        $role->description = $description;
-
-        try {
-            Yii::$app->authManager->remove($role);
-        } catch (Exception $e) {
-            Yii::error($e->getMessage(), '_createRole');
-        }
-    }
-
-
-
-    public function safeUp() {
-        $this->createRoles();
-        $this->createPermissions();
-        $this->assignAdminPermissions();
-        $this->assignNewRolePermissions();
-    }
-
-    public function safeDown() {
-        $this->deleteRoles();
-    }
-}
