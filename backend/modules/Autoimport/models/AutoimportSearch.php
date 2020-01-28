d845c14d8cac2b0e8da584659e95bd5c6466aad0
orbanisti <orbanisti94@gmail.com>
2020-01-28 13:23:00 +0100

    hotfix

--- backend/modules/Autoimport/models/AutoimportSearch.php
+++ backend/modules/Autoimport/models/AutoimportSearch.php
@@ -14,6 +14,7 @@ class AutoimportSearch extends Autoimport
 {
     public function rules()
     {
+
         return [
             [['id', 'active'], 'integer'],
             [['siteUrl', 'apiUrl', 'type'], 'safe'],
